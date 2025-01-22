4c702c996894f4fedb015ce6799504c955a09bf0
RubenMaestre <formacion@rubenmaestre.com>
2025-01-22 17:45:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 # app.py
 from modules.config_page import set_global_page_config
 import streamlit as st
-from modules.create_sidebar import create_sidebar
 
-# Configuración global de la página
+# Llama a set_page_config al inicio y asegúrate de que es el primer comando de Streamlit usado en el script.
 set_global_page_config()
 
-# Barra lateral y menú
+# Importaciones adicionales y lógica de la aplicación aquí
+from modules.create_sidebar import create_sidebar
+
+# Otros comandos de Streamlit y funciones deben ir después de set_page_config()
 create_sidebar()
+
+
+
 
