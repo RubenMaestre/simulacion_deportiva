68d4419532223ccf6d5c9979253ef4d0a648922c
RubenMaestre <formacion@rubenmaestre.com>
2025-01-22 17:53:26 +0100

    solucion_menu

--- app.py
+++ app.py
@@ -1,16 +1,11 @@
-# app.py
 from modules.config_page import set_global_page_config
+from modules.create_sidebar import create_sidebar
 import streamlit as st
 
-# Llama a set_page_config al inicio y asegúrate de que es el primer comando de Streamlit usado en el script.
+# Configuración global de la página
 set_global_page_config()
 
-# Importaciones adicionales y lógica de la aplicación aquí
-from modules.create_sidebar import create_sidebar
-
-# Otros comandos de Streamlit y funciones deben ir después de set_page_config()
+# Crear la barra lateral y redirigir al contenido correspondiente
 create_sidebar()
-
-
 
 
--- modules/create_sidebar.py
+++ modules/create_sidebar.py
@@ -1,15 +1,23 @@
 import streamlit as st
 
-# Crear un menú básico en el sidebar
-menu = st.sidebar.radio(
-    "Menú",
-    ["Inicio", "Datos", "EDA", "Modelo", "Sobre el proyecto", "Sobre nosotros"]
-)
+def create_sidebar():
+    # Crear un menú básico en el sidebar
+    menu = st.sidebar.radio(
+        "Menú",
+        ["Inicio", "Datos", "EDA", "Modelo", "Sobre el proyecto", "Sobre nosotros"]
+    )
 
-# Redirigir según la selección
-if menu == "Inicio":
-    st.write("Página de inicio")
-elif menu == "Datos":
-    st.write("Página de datos")
-# Agrega más opciones según sea necesario
+    # Redirigir según la selección
+    if menu == "Inicio":
+        st.write("Página de inicio")
+    elif menu == "Datos":
+        st.write("Página de datos")
+    elif menu == "EDA":
+        st.write("Análisis Exploratorio de Datos (EDA)")
+    elif menu == "Modelo":
+        st.write("Página del modelo")
+    elif menu == "Sobre el proyecto":
+        st.write("Información sobre el proyecto")
+    elif menu == "Sobre nosotros":
+        st.write("Información sobre nosotros")
 
